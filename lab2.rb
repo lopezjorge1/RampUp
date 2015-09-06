10f66ab0883967ee40d04a8c70281d01b4c83807
Jorge <lopez.jorge1@outlook.com>
2015-09-06 16:46:20 -0400

    rampup labs

--- lab2.rb
+++ lab2.rb
@@ -1,8 +1,9 @@
 def bottles_of_beer
+	#Takes a range of numbers and reverses each of them, then changes them so that they're included in string
 	(2..99).reverse_each.map do |x| 
 		print """\n#{x} bottles of beer on the wall, #{x} bottles of beer.\n Take one down and pass it around, #{x-1} bottles of beer on the wall."""
 	end
-
+	#This string is a bit different for the last two numbers, but does same thing
 	(0..1).reverse_each.map do |x|
 		print """\n#{x} bottle of beer on the wall, #{x} bottle of beer.\n Take one down and pass it around, no more bottles of beer on the wall.""" if x == 1
 		print """\nNo more bottles of beer on the wall, no more bottles of beer.\n Go to the store and buy some more, 99 bottles of beer on the wall.""" if x == 0
@@ -17,6 +18,7 @@ def deaf_grandma
 		response = gets.chomp
 		if response == response.upcase 
 			response_arr.push(response)
+			#Breaks if there are three consectutive strings that each equal "BYE"
 			break if response_arr.each_cons(3).any? {|x,y,z| x == "BYE" && y == "BYE" && z == "BYE"} and p "Bye Sonny!"
 			p "No, no since #{rand(1930..1980)}!"
 		else
