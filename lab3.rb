2935ed64918929409a592f5fe3c35ede6198ab6d
Jorge <lopez.jorge1@outlook.com>
2015-09-24 11:29:34 -0400

    made some corrections

--- lab3.rb
+++ lab3.rb
@@ -1,13 +1,13 @@
 def crypt(choice, str)
-	cryption = {a: "$",b: "!",c: "#",d: "@",e: "%",f: "^",g: "^",h: "&",i: "*",j: "(",k: ")", 
+	cryption = {a: "$",b: "!",c: "#",d: "@",e: "%",f: "^",g: "-",h: "&",i: "*",j: "(",k: ")", 
 				l: "{",m: "}",n: "[",o: "]",p: "|",q: ",",r: ".",s: "<",t: ">", u: "/", v: "?",
 				w: "~",x: "`",y: "+",z: "="}
 	#Take the string and encrypts it using the symbols in the hash
 	if choice == "en"
-		str.split(//).map {|x| x == " " ? x = " " : x = cryption[x.to_sym]}.join
+		str.downcase.chars.map {|x| x == " " ? x = " " : x = cryption[x.to_sym]}.join
 	#Or decrypts it
 	elsif choice == "de"
-		str.split(//).map {|x| x == " " ? x = " " : x = cryption.key(x).to_s}.join
+		str.chars.map {|x| x == " " ? x = " " : x = cryption.key(x).to_s}.join
 	end
 end
 
@@ -21,3 +21,4 @@ def caesar_cipher(direction,num,str)
 		str.split(//).map {|x| x == " " ? x = " " : x = (x.ord - num).chr}.join
 	end
 end
+
