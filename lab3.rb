2b83d63cec88a4e997c6f98875448feb7cab6a2c
Jorge <lopez.jorge1@outlook.com>
2015-09-24 09:26:53 -0400

    lab 3 completed

--- lab3.rb
+++ lab3.rb
@@ -1,7 +1,12 @@
-=begin
-def encrypt(str)
-	ascii = str.split(//).map {|x| x.ord}
-	
+def crypt(way, str)
+	cryption = {a: "$",b: "!",c: "#",d: "@",e: "%",f: "^",g: "^",h: "&",i: "*",j: "(",k: ")", 
+				l: "{",m: "}",n: "[",o: "]",p: "|",q: ",",r: ".",s: "<",t: ">", u: "/", v: "?",
+				w: "~",x: "`",y: "+",z: "="}
+	#Take the string and encrypts it using the symbols in the hash
+	if way == "en"
+		str.split(//).map {|x| x == " " ? x = " " : x = cryption[x.to_sym]}.join
+	#Or decrypts it
+	else 
+		str.split(//).map {|x| x == " " ? x = " " : x = cryption.key(x).to_s}.join
+	end
 end
-encrypt("hello")
-=end
