389dca3d5427989bce7e928c974893588e368f21
Jorge <lopez.jorge1@outlook.com>
2015-09-24 09:59:12 -0400

    added caesar cipher

--- lab3.rb
+++ lab3.rb
@@ -1,12 +1,23 @@
-def crypt(way, str)
+def crypt(choice, str)
 	cryption = {a: "$",b: "!",c: "#",d: "@",e: "%",f: "^",g: "^",h: "&",i: "*",j: "(",k: ")", 
 				l: "{",m: "}",n: "[",o: "]",p: "|",q: ",",r: ".",s: "<",t: ">", u: "/", v: "?",
 				w: "~",x: "`",y: "+",z: "="}
 	#Take the string and encrypts it using the symbols in the hash
-	if way == "en"
+	if choice == "en"
 		str.split(//).map {|x| x == " " ? x = " " : x = cryption[x.to_sym]}.join
 	#Or decrypts it
-	else 
+	elsif choice == "de"
 		str.split(//).map {|x| x == " " ? x = " " : x = cryption.key(x).to_s}.join
+	end
+end
+
+
+def caesar_cipher(direction,num,str)
+	if direction == "down" 
+		#this will just shift letters down, so A --> Z
+		str.chars.map {|x| x == " " ? x = " " : x = (x.ord + num).chr}.join
+	elsif direction == "up" 
+		#this will shift letters up, so Z --> A
+		str.split(//).map {|x| x == " " ? x = " " : x = (x.ord - num).chr}.join
 	end
 end
