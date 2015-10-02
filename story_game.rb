8e5fe469c6cc0db33cf49d0318b2f4c94ff401eb
Jorge <lopez.jorge1@outlook.com>
2015-10-02 10:54:32 -0400

    tried out a fibonacci sequence

--- story_game.rb
+++ story_game.rb
@@ -1,4 +1,5 @@
 #this method is supposed to be a conditional for whatever response is given to story line
+=begin
 def story_game
 	puts "You’re a traveler on a long journey. After many miles, you come to a fork in the road.\nTo the North is a small village. To the East is dark cave.\nWhich way do you go? North or East?"
 	response = gets.chomp.downcase
@@ -7,4 +8,13 @@ def story_game
 	elsif response == "north"
 		puts "You went the wrong way. The village kills you."
 	end
+end
+=end
+
+def fibonacci(num)
+	arr = [1,2]
+	until arr.last >= num
+		arr.push(arr.last(2).inject {|sum,x| sum += x})
+	end
+	arr.inject{|sum,x| x % 2 == 0 ? sum += x : sum += 0}
 end
